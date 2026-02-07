299fa7ebd12b022ecdf1755b9e6dc95e9288e295
keeganhall47-ops <keeganhall47@gmail.com>
2026-02-06 21:53:20 -0500

    Loading Screen

--- buzz-frontend/src/App.tsx
+++ buzz-frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import './App.css'
 import LoadingScreen from './screens/LoadingScreen'
 import MainMapScreen from './screens/MainMapScreen'
@@ -6,16 +5,15 @@ import ProfileScreen from './screens/ProfileScreen'
 import { useAppState } from './hooks/useAppState'
 
 function App() {
-  const { currentScreen, finishLoading } = useAppState()
+  const { currentScreen } = useAppState()
 
-  // Simulate loading time
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      finishLoading()
-    }, 3000) // 3 second loading screen
-
-    return () => clearTimeout(timer)
-  }, [finishLoading])
+  // Simulate loading time (disabled so LoadingScreen stays visible)
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     finishLoading()
+  //   }, 3000) // 3 second loading screen
+  //   return () => clearTimeout(timer)
+  // }, [finishLoading])
 
   // Render different screens based on current state
   switch (currentScreen) {
